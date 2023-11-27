30ec2616ffff9177abab78bbf011979fbe4b26ec
LAPTOP-QMSU3DDF\tzuli <tzulikuo@gmail.com>
2023-11-27 14:30:03 +0800

    add requirements.txt

--- log_config.py
+++ log_config.py
@@ -4,10 +4,8 @@ from logging.handlers import TimedRotatingFileHandler
 
 
 class Log():
-    def set_log(self, filepath = "logs/log.log", level = 2, freq = "D", interval = 50, backup = 2, name = "log"):
-        format = '%(asctime)s %(levelname)s %(message)s'
-        datefmt = '%Y-%m-%d %H:%M:%S'
-        level_dict = {
+    def __init__(self):
+        self.level_dict = {
             1: logging.DEBUG,
             2: logging.INFO,
             3: logging.ERROR,
@@ -15,20 +13,29 @@ class Log():
             5: logging.CRITICAL,
         }
 
-        fmt = logging.Formatter(format, datefmt)
 
-        log_level = level_dict[level]
+    def set_log(self, filepath: str = "logs/log.log", level: int = 2, freq: str = "D", interval: int = 50, backup: int = 2, name: str = "log"):
+        # define log format and date format
+        format_  = "%(asctime)s %(levelname)s %(message)s"
+        time_format = "%Y-%m-%d %H:%M:%S"
 
+        formatter = logging.Formatter(format_, time_format) # create a log formatter
+        log_level = self.level_dict[level] # get log level based on the provided "level"
+
+        # initialize the logger
         self.logger = logging.getLogger(name = name)
         self.logger.setLevel(log_level)
 
-        self.hdlr = TimedRotatingFileHandler(filename = filepath, when = freq, interval = interval, backupCount = backup, encoding='utf-8')
-        self.hdlr.setFormatter(fmt)
-        self.logger.addHandler(self.hdlr)
+        # create a file handler for log rotation
+        self.handler = TimedRotatingFileHandler(filename = filepath, when = freq, interval = interval, backupCount = backup, encoding = "utf-8")
+        self.handler.setFormatter(formatter)
+
+        # add the file handler to the logger
+        self.logger.addHandler(self.handler)
 
         return self.logger
     
 
     def shutdown(self):
-        self.logger.removeHandler(self.hdlr)
-        del self.logger, self.hdlr
+        self.logger.removeHandler(self.handler)  # remove log handlers
+        del self.logger, self.handler # delete logger instances
